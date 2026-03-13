d3c2dd88bd36349fa9219d0fd2dee57f90bbf63b
Stel-00 <stel@poli.cc>
2026-03-13 23:18:15 +0100

    Fixes #1

--- hashish.py
+++ hashish.py
@@ -1,48 +1,37 @@
-def list_data():
-    print("id, item, price per kg")
-    with open("hash.txt", "r") as file:
-        print(file.read())
+class HashMap:
+    def __init__(self, n=8):
+        self.size = n
+        self.count = 0
+        self.map = [[] for _ in range(n)]
 
-def query(id):
-    with open("hash.txt", "r") as file:
-        for line in file:
-            tab = line.split(", ")
-            if tab[0] == str(id).zfill(2):
-                print("id, item, price")
-                print(line)
-                return None
-        print("error, item not found")
+    def _hash(self, key):
+        return hash(key) % self.size #Converts the key into an integer whaterver the type
 
-def add_data(id, item, price):
-    if not in_db():
-        with open("hash.txt", "a") as file:
-            file.write(f"\n{str(id).zfill(2)}, {item}, {price}\n")
-    else:
-        with open("hash.txt", "r") as file:
-        for line in file:
-            tab = line.split(", ")
-            if tab[0] == str(id).zfill(2):
-                tab[]
-                return None
+    def hash_add(self, key, value):
+        index = self._hash(key)
 
-def in_db(id):
-    with open("hash.txt", "r") as file:
-        for line in file:
-            tab = line.split(", ")
-            if tab[0] == str(id).zfill(2):
-                return True
-    return False
+        for i, (k, v) in enumerate(self.map[index]):
+            if k == key:
+                self.map[index][i] = (key, value)
+                return
 
-def remove_data(id):
-    with open("hash.txt", "r") as file:
-        lines = file.readlines()
+        self.map[index].append((key, value))
+        self.count += 1
 
-    with open("hash.txt", "w") as file:
-        for line in lines:
-            if line[0] != target:
-                f.write(line)
+        if self.count / self.size > 0.7: #If load factor is over 70% we resize
+            self.resize()
 
+    def resize(self):
+        old_map = self.map
+        self.size *= 2 #Double the size of the map
+        self.map = [[] for _ in range(self.size)]
+        self.count = 0
 
-add_data(88,"letus","16kr")
-list_data()
+        for bucket in old_map:
+            for k, v in bucket:
+                self.hash_add(k, v)
 
+    def list_all(self):
+        for bucket in self.map:
+            for k, v in bucket:
+                print(k, v)
